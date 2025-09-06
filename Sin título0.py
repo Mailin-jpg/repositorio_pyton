f58bdb46015ae7c0c8d9bd7e16be4f583bfc29b2
Mailin Silva <mailinsilvamanjarrez@gmail.com>
2025-09-06 18:13:13 -0500

    operacion resta lista

--- Sin título0.py
+++ Sin título0.py
@@ -4,9 +4,11 @@ Created on Sat Sep  6 17:56:03 2025
 
 @author: SALA-1
 """
-numero_uno= 10
-numero_dos=5
+numero_uno = 10
+numero_dos =5
 
 suma=numero_uno+numero_dos
-print ('suma=',suma)
+print ('suma =',suma)
 
+resta= numero_uno - numero_dos
+print('resta =' , resta)
